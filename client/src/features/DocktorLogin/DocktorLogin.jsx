6b0380edf098f98cb5de05476707512ff80bde9e
A. Feyzullah YILDIZ <feyzullah.yildiz@orioninc.com>
2022-11-16 19:18:41 +0300

    Private route logic

---
+++ client/src/features/DocktorLogin/DocktorLogin.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DocktorLogin = () => {
+  return (
+    <div>DocktorLogin</div>
+  )
+}
--- client/src/features/Layout.js
+++ client/src/features/Layout.js
@@ -6,11 +6,23 @@ import NavBar from "./common/NavBar";
 import Sidebar from "./common/Sidebar";
 
 import { Dashboard } from "./index";
+import { Login } from "./Login/Login";
+import { DocktorLogin } from "./DocktorLogin/DocktorLogin";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/login-as-doctor" element={<DocktorLogin />} />
+    </Routes>
+  );
+};
+const PublicRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/login-as-doctor" element={<DocktorLogin />} />
     </Routes>
   );
 };
@@ -19,19 +31,21 @@ function Layout() {
   const navbarState = useSelector((state) => state.navbar);
   const isOpen = navbarState.isOpen;
 
-  const isAuth = true;
+  const isAuth = false;
   return (
     <div className="m-0 p-0 bg-contrast-5">
-      <NavBar isOpen={isOpen} />
       {!isAuth ? (
-        <div>Please login</div>
+        <>{PublicRoutes()}</>
       ) : (
-        <div className="flex">
-          <aside className="self-start sticky top-0">
-            <Sidebar isShowing={isOpen} />
-          </aside>
-          <main className="w-full overflow-y-auto">{AppRoutes()}</main>
-        </div>
+        <>
+          <NavBar isOpen={isOpen} />
+          <div className="flex">
+            <aside className="self-start sticky top-0">
+              <Sidebar isShowing={isOpen} />
+            </aside>
+            <main className="w-full overflow-y-auto">{AppRoutes()}</main>
+          </div>
+        </>
       )}
       <ToastContainer />
     </div>
