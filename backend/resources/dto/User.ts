22e00247b3757ab3f2303cad092c9bf46a935a07
Mustafa Onan <mustafa.onan@orioninc.com>
2022-11-26 16:12:17 +0300

    update user dto

--- backend/resources/dto/User.ts
+++ backend/resources/dto/User.ts
@@ -1,9 +1,6 @@
-export interface User {
+import { UserInput } from "./RegisterEvent";
+export interface User extends UserInput {
   id: string;
-  first_name: string;
-  last_name: string;
-  email: string;
-  password: string;
   created_at: Date;
   updated_at: Date;
 }
