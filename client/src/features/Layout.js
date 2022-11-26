72a15fde2127bd1847191827b981fcb7a48d2a4f
Vladimir <vladimir.ermolaev@orioninc.com>
2022-11-26 13:31:04 +0000

    [vladimir] added protected route

--- client/src/features/Layout.js
+++ client/src/features/Layout.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -7,40 +8,50 @@ import Sidebar from "./common/Sidebar";
 
 import { Dashboard, ExamplePage, Login } from "./index";
 import { DoctorLogin } from "./Login/DoctorLogin";
+import ProtectedRoute from "./ProtectedRoute";
 import { Register } from "./Register/Register";
 import { SignOut } from "./SignOut/SignOut";
 
-const AppRoutes = () => {
+const Mock = (props) => {
+  console.log("mock props", props);
+  return <h1>PRIVATE</h1>;
+};
+
+const AppRoutes = (loggedIn, setLoggedIn) => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/example" element={<ExamplePage />} />
+      <Route exact path="/" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/login-as-doctor" element={<DoctorLogin />} />
       <Route path="/register" element={<Register />} />
       <Route path="/signout" element={<SignOut />} />
+      <Route path="/example" element={<ExamplePage />} />
+      <Route
+        path="/private"
+        element={<ProtectedRoute component={Mock} loggedIn={loggedIn} />}
+      />
     </Routes>
   );
 };
 
 function Layout() {
+  const [loggedIn, setLoggedIn] = useState(false);
   const navbarState = useSelector((state) => state.navbar);
   const isOpen = navbarState.isOpen;
-//bg-contrast-5
-  const isAuth = false;
+  //bg-contrast-5
+  const isAuth = true;
   return (
     <div className="m-0 p-0">
       <NavBar isOpen={isOpen} isAuth={isAuth} />
-      {!isAuth ? (
-        <div>Please login</div>
-      ) : (
-        <div className="flex">
-          <aside className="self-start sticky top-0">
-            <Sidebar isShowing={isOpen} />
-          </aside>
-          <main className="w-full overflow-y-auto">{AppRoutes()}</main>
-        </div>
-      )}
+      <div className="flex">
+        <aside className="self-start sticky top-0">
+          <Sidebar isShowing={isOpen} />
+        </aside>
+        <main className="w-full overflow-y-auto">
+          {AppRoutes(loggedIn, setLoggedIn)}
+        </main>
+      </div>
+
       <ToastContainer />
     </div>
   );
